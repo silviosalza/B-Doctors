7fa7e3e0c423691818d17d22a4f59654965150a6
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-10-18 10:23:49 +0200

    add_model_relations

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     use HasFactory;
+
+    public function typologies()
+    {
+        return $this->belongsToMany(Typology::class);
+    }
+
+    public function stars()
+    {
+        return $this->belongsToMany(Star::class);
+    }
 }
