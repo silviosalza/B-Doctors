7148b66eb587e1657b9f0990f1fe274185f9b1bb
Luigi213 <reyesgianluigi88@gmail.com>
2023-10-18 10:43:39 +0200

    done doctor seeder

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Doctor extends Model
 {
     use HasFactory;
+
+    public static function generateSlug($name){
+        return Str::slug($name, '-');
+    }
 }
