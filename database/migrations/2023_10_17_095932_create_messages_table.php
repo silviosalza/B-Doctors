c22a1e5289c6506ddc3b3192b4fd4eee27f2a133
MLibralato91 <michelelibralato911@gmail.com>
2023-10-18 11:25:45 +0200

    fix table

--- database/migrations/2023_10_17_095932_create_messages_table.php
+++ database/migrations/2023_10_17_095932_create_messages_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->on('users')->references('id')->onDelete('cascade');
+            $table->unsignedBigInteger('doctor_id');
+            $table->foreign('doctor_id')->on('doctors')->references('id')->onDelete('cascade');
             $table->string('name_ui', 30);
             $table->string('lastname_ui', 30);
             $table->string('email_ui', 50);
-            $table->text('text', 500); 
+            $table->text('text', 500);
             $table->timestamps();
         });
     }
