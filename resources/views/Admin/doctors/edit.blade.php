c2dcf31181d472608c1b91d052851a7273b46a71
MLibralato91 <michelelibralato911@gmail.com>
2023-10-20 12:26:46 +0200

    update edit.blade

--- resources/views/Admin/doctors/edit.blade.php
+++ resources/views/Admin/doctors/edit.blade.php
@@ -1,29 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <form>
-        <div class="container">
+    <div class="container">
+        <form method="POST" action="{{ route('admin.doctors.update', $doctor) }}" enctype="multipart/form-data">
+            @method('PUT')
+            @csrf
             <div class="row">
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Indirizzo</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                            name="address">
+                        <label for="exampleInputAddress" class="form-label">Indirizzo</label>
+                        <input type="address" class="form-control" id="exampleInputAddress" aria-describedby="addressHelp"
+                            name="address" value="{{ $doctor->address }}">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Descrizione</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" name="description">
+                        <label for="exampleInputDescription" class="form-label">Descrizione</label>
+                        <textarea id="type" name="description" class="form-control" id="exampleInputDescription" rows="5">{{ $doctor->description }}</textarea>
+
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Foto</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" name="photo">
+                        <label for="exampleInputPhoto" class="form-label">Foto</label>
+                        <input type="text" class="form-control" id="exampleInputPhoto" name="photo"
+                            value="{{ $doctor->photo }}">
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Servizio</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                            name="service">
+                        <label for="exampleInputService" class="form-label">Servizio</label>
+                        <input type="text" class="form-control" id="exampleInputService" aria-describedby="emailHelp"
+                            name="service" value="{{ $doctor->services }}">
                     </div>
                     <div class="mb-3">
                         <div class="form-check">
@@ -34,7 +38,7 @@
                         </div>
                     </div>
                     <div class="mb-3">
-                        <h6>Specializzazione/i</h6>
+                        <label for="exampleInputTypologies" class="form-label">Specializzazione</label>
                         @foreach ($typologies as $typology)
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" id="disabledFieldsetCheck"
@@ -51,7 +55,10 @@
             </div>
 
 
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </form>
+            <div class="d-flex align-items-center justify-content-center mt-4 mb-0">
+                <button class="btn btn-secondary me-2" type="reset">Reset</button>
+                <button class="btn btn-primary ms-2" type="submit">Modifica</button>
+            </div>
+        </form>
+    </div>
 @endsection
