a33af812ccaa212913e8758f19e53316579172bd
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-10-26 10:48:32 +0200

    add typologies api call

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Doctor;
+use App\Models\Typology;
 use App\Models\User;
 
 class DoctorController extends Controller
 {
     public function index(){
         $doctors = Doctor::with('user')->get();
+
+        $typologies = Typology::all();
         return response()->json([
             'success' => true,
             'results' => $doctors,
+            'typologies' => $typologies,
         ]);
     }
 
